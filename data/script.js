899c9f286d58d325775c0d4c2addfc23ad62c24b
Soxa <sora@soxa.dev>
2024-02-25 00:55:45 +0100

    localStorage for theme

--- data/script.js
+++ data/script.js
@@ -13,15 +13,27 @@ function egg() {
 }
 
 var alternate = 0;
-function alternateBg() {
-    if (alternate==0) {
+if(localStorage.getItem("alternate") === null) {
+    localStorage.setItem("alternate", alternate);
+}
+
+refreshBg(alternate);
+
+function refreshBg() {
+    alternate = parseInt(localStorage.getItem("alternate"));
+    setBg(alternate);
+}
+
+function setBg(bg) {
+    if (bg == 0) {
         document.body.style.backgroundImage = "url('../img/bg-alternate.jpeg')";
-        document.getElementById('alternate').innerHTML = `<i id="alternator" class="bi bi-brightness-alt-high" onclick="alternateBg()"> night</i>`;
-        alternate=1;
+        document.getElementById('alternate').innerHTML = `<i id="alternator" class="bi bi-brightness-alt-high" onclick="setBg(1)"> night</i>`;
+        alternate = 0;
     } else {
         document.body.style.backgroundImage = "url('../img/bg.jpg')";
-        document.getElementById('alternate').innerHTML = `<i id="alternator" class="bi bi-brightness-alt-high-fill" onclick="alternateBg()"> day</i>`;
-        alternate=0;
+        document.getElementById('alternate').innerHTML = `<i id="alternator" class="bi bi-brightness-alt-high-fill" onclick="setBg(0)"> day</i>`;
+        alternate = 1;
     }
-
+    localStorage.setItem("alternate", alternate);
 }
+
